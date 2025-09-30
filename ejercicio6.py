8d1b7d83fd316347514cd27fd2c805726e048a07
javilillo <japadial11@gmail.com>
2025-09-30 14:17:03 +0200

    Ejercicio 6

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,4 +1,8 @@
-peso = float(input("Peso (kg): ")) # Pedimos el peso, y con float convertimos el peso a numero decimal
-altura = float(input("Altura (m): ")) # Pedimos la altura y con float convertimos a numero decimal
-imc = peso / (altura ** 2) # Decimos que el IMC es la altura elevado a 2 entre el peso
-print(f"IMC: {imc}") # Mostramos el IMC
+# Leer un entero positivo del usuario
+n = int(input("Introduce un número entero positivo: "))
+
+# Calcular la suma de 1 hasta n usando la fórmula matemática
+suma = n * (n + 1) // 2 # La // es el consciente
+
+# Mostrar el resultado
+print(f"La suma de todos los enteros desde 1 hasta {n} es: {suma}")
---
+++ ejercicio7.py
@@ -0,0 +1,4 @@
+peso = float(input("Peso (kg): ")) # Pedimos el peso, y con float convertimos el peso a numero decimal
+altura = float(input("Altura (m): ")) # Pedimos la altura y con float convertimos a numero decimal
+imc = peso / (altura ** 2) # Decimos que el IMC es la altura elevado a 2 entre el peso
+print(f"IMC: {imc}") # Mostramos el IMC
