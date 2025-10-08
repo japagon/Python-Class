9009e6d67f02860691eda0230b5dfa3135249bc8
javilillo <japadial11@gmail.com>
2025-10-08 10:02:26 +0200

    Ejercicio 11

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,6 +1,6 @@
 # Programa para calcular el saldo de una cuenta de ahorros con interés compuesto
 
-deposito = float(input("Introduce la cantidad de dinero depositada en la cuenta de ahorros: "))
+deposito = float(input("Pon la cantidad de dinero depositada en la cuenta de ahorros: "))
 interes = 0.04
 
 saldo_1 = round(deposito * (1 + interes), 2)
@@ -9,4 +9,5 @@ saldo_3 = round(saldo_2 * (1 + interes), 2)
 
 print(f"Saldo tras el primer año: {saldo_1}")
 print(f"Saldo tras el segundo año: {saldo_2}")
-print(f"Saldo tras el tercer año: {saldo_3}")
+print(f"Saldo tras el tercer año: {saldo_3}") 
+ 
