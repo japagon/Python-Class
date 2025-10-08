c73d0cab2ecbb2ced722da57622bbee86fd40849
javilillo <japadial11@gmail.com>
2025-10-08 10:17:35 +0200

    Ejercicio 12

--- ejercicio12.py
+++ ejercicio12.py
@@ -1,7 +1,7 @@
 BarraDePan = 3.49
 Descuento = 0.60
 
-NumeroDeBarras = int(input("De barras que no son del dia se han vendido: "))
+NumeroDeBarras = int(input("Introduce el numero de barras vendidas: "))
 
 PrecioConDescuento = BarraDePan - Descuento
 Total = NumeroDeBarras * PrecioConDescuento
@@ -10,4 +10,4 @@ Total = NumeroDeBarras * PrecioConDescuento
 
 print(f"El precio habitual es {BarraDePan:.2f} euros")
 print(f"Con el descuento al no ser del dia se queda en: {PrecioConDescuento:.2f}")
-print(f"El total a pagar por el {NumeroDeBarras} es {Total:.2f}.")
+print(f"El total a pagar por el {NumeroDeBarras} es {Total:.2f}.") 
