fb16fcf7e660170b158de5dce230c76cb091fcdd
javilillo <japadial11@gmail.com>
2025-10-21 13:38:57 +0200

    Ejercicio 3 | Cadena de caracteres

---
+++ CADENAS DE CARACTERES/ejercicio3.py
@@ -0,0 +1,8 @@
+# Ejercicio 3
+# Escribir un programa que pregunte el nombre del usuario en la consola y después
+# de que el usuario lo introduzca muestre por pantalla <NOMBRE> tiene <n> letras
+
+nombre = input("Introduce tu nombre: ")
+print(f"{nombre.upper()} tiene {len(nombre)} letras")
+
+print("-" * 30)
