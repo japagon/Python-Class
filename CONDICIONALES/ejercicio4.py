1c716291fe33dc9ad087372e1bd88911432e470e
javilillo <japadial11@gmail.com>
2025-10-21 14:29:33 +0200

    Ejercicio 4 | CONDICIONALES

---
+++ CONDICIONALES/ejercicio4.py
@@ -0,0 +1,10 @@
+# Ejercicio 4
+# Escribir un programa que pida al usuario un 
+# número entero y muestre por pantalla si es par o impar.
+
+num = int(input("Añade el numero: "))
+
+if num % 2 == 0:
+    print("El numero es par")
+else :
+    print("El numero es impar")
