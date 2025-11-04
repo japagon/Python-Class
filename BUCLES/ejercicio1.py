102173d45ca18e37517f9823af2c9bc0c1c49449
javilillo <japadial11@gmail.com>
2025-11-04 14:13:38 +0100

    Ejercicio 1 BUCLES

---
+++ BUCLES/ejercicio1.py
@@ -0,0 +1,8 @@
+# Ejercicio 1
+# Escribir un programa que pida al usuario una palabra
+# y la muestre por pantalla 10 veces.
+
+palabra = input("Escribe una palabra: ")
+
+for i in range(10):
+    print(palabra)
