dbb1969d7bb14bc4388603915992fbe80b8322f4
javilillo <japadial11@gmail.com>
2025-11-11 14:22:39 +0100

    Ejercicio 8 BUCLES

---
+++ BUCLES/ejercicio8.py
@@ -0,0 +1,14 @@
+# Ejercicio 8
+# Escribir un programa que pida al usuario un número entero y muestre por pantalla
+# un triángulo rectángulo como el de más abajo.
+# 1
+# 3 1
+# 5 3 1
+# 7 5 3 1
+# 9 7 5 3 1
+numero = int(input("Introduce un numero entero: "))
+
+for i in range(1, numero + 1, 2):
+    print()
+    for j in range(i, 0, - 2):
+        print(j, end=" ")
